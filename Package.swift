3dbb336333c60d852bcda99b37bbd4714e50d4d3
Tibor Bödecs <mail.tib@gmail.com>
2020-12-21 18:03:58 +0100

    removed feather target

--- Package.swift
+++ Package.swift
@@ -11,14 +11,15 @@ let package = Package(
         .library(name: "SystemModuleApi", targets: ["SystemModuleApi"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
-        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
         .package(url: "https://github.com/FeatherCMS/feather-core", from: "1.0.0-beta"),
+        /// drivers
+//        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
+//        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
         /// core modules
-        .package(url: "https://github.com/FeatherCMS/user-module", from: "1.0.0-beta"),
-        .package(url: "https://github.com/FeatherCMS/api-module", from: "1.0.0-beta"),
-        .package(url: "https://github.com/FeatherCMS/admin-module", from: "1.0.0-beta"),
-        .package(url: "https://github.com/FeatherCMS/frontend-module", from: "1.0.0-beta"),
+//        .package(url: "https://github.com/FeatherCMS/user-module", from: "1.0.0-beta"),
+//        .package(url: "https://github.com/FeatherCMS/api-module", from: "1.0.0-beta"),
+//        .package(url: "https://github.com/FeatherCMS/admin-module", from: "1.0.0-beta"),
+//        .package(url: "https://github.com/FeatherCMS/frontend-module", from: "1.0.0-beta"),
     ],
     targets: [
         .target(name: "SystemModuleApi", dependencies: []),
@@ -29,20 +30,21 @@ let package = Package(
         resources: [
             .copy("Bundle"),
         ]),
-        .target(name: "Feather", dependencies: [
-            .product(name: "FeatherCore", package: "feather-core"),
-            
-            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
-            
-            /// core modules
-            .product(name: "UserModule", package: "user-module"),
-            .product(name: "ApiModule", package: "api-module"),
-            .product(name: "AdminModule", package: "admin-module"),
-            .product(name: "FrontendModule", package: "frontend-module"),
-            
-            .target(name: "SystemModule"),
-        ]),
+//        @NOTE: https://bugs.swift.org/browse/SR-8658
+//        .target(name: "Feather", dependencies: [
+//            .product(name: "FeatherCore", package: "feather-core"),
+//
+//            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+//            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
+//
+//            /// core modules
+//            .product(name: "UserModule", package: "user-module"),
+//            .product(name: "ApiModule", package: "api-module"),
+//            .product(name: "AdminModule", package: "admin-module"),
+//            .product(name: "FrontendModule", package: "frontend-module"),
+//
+//            .target(name: "SystemModule"),
+//        ]),
         .testTarget(name: "SystemModuleTests", dependencies: [
             .target(name: "SystemModule"),
         ])
